afe5ef088563e2366f48b2546e218cc186c11977
parthjpatel99 <parth8199@gmail.com>
2022-02-20 16:43:06 -0500

    Parsed JSON Response to Movie data class

--- app/src/main/java/com/parth8199/flixter/MainActivity.kt
+++ app/src/main/java/com/parth8199/flixter/MainActivity.kt
@@ -6,16 +6,21 @@ import android.util.Log
 import com.codepath.asynchttpclient.AsyncHttpClient
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import okhttp3.Headers
+import org.json.JSONException
 
 private const val TAG = "MainActivity"
-private const val NOW_PLAYING_URL="https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+private const val NOW_PLAYING_URL =
+    "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+
 class MainActivity : AppCompatActivity() {
+    private val movies = mutableListOf<Movie>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val client = AsyncHttpClient()
-        client.get(NOW_PLAYING_URL, object : JsonHttpResponseHandler(){
+        client.get(NOW_PLAYING_URL, object : JsonHttpResponseHandler() {
             override fun onFailure(
                 statusCode: Int, headers: Headers?, response: String?, throwable: Throwable?
             ) {
@@ -23,8 +28,14 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
-                Log.i(TAG,"onSuccess: JSON data $json")
-                val movieJsonArray = json.jsonObject.getJSONArray("results")
+                Log.i(TAG, "onSuccess: JSON data $json")
+                try {
+                    val movieJsonArray = json.jsonObject.getJSONArray("results")
+                    movies.addAll(Movie.fromJsonArray(movieJsonArray))
+                    Log.i(TAG, "Movie List $movies")
+                }catch (e:JSONException){
+                    Log.e(TAG, "Exception Encountered $e")
+                }
             }
 
         })
